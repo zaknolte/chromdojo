6b8148efcc10ca7cc87e178e3d6e614bda93d412
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-07-04 13:36:27 -0700

    update namespace and fix y calc

--- assets/Peak.js
+++ assets/Peak.js
@@ -22,91 +22,96 @@ class Compound {
   }
 
   create_peak(x) {
-      // current formula with skewness does not draw with width = 0
-      // set to minimal sharp value to improve UX and not have peaks disappearing
-      if (this.width === 0) {
-          this.width = 0.01;
-      }
-      // https://www.desmos.com/calculator/gokr63ciym
-      return this.height * Math.exp(-0.5 * ((x - this.center) / (this.width + (this.skew * (x - this.center))))**2);
-      // https://www.desmos.com/calculator/k5y9glwjee   ??
-      // https://math.stackexchange.com/questions/3605861/what-is-the-graph-function-of-a-skewed-normal-distribution-curve
-      // https://cremerlab.github.io/hplc-py/methodology/fitting.html
+    // current formula with skewness does not draw with width = 0
+    // set to minimal sharp value to improve UX and not have peaks disappearing
+    if (this.width === 0) {
+        this.width = 0.01;
+    }
+    // https://www.desmos.com/calculator/gokr63ciym
+    var y = Array(x.length).fill(0);
+    var calcY = y.map((num, idx) => {
+        return this.height * Math.exp(-0.5 * ((x[idx] - this.center) / (this.width + (this.skew * (x[idx] - this.center))))**2);
+        // this.height * Math.exp(-0.5 * ((x[idx] - this.center) / (this.width + (this.skew * (x[idx] - this.center))))**2)
+    });
+    return calcY;
+    // https://www.desmos.com/calculator/k5y9glwjee   ??
+    // https://math.stackexchange.com/questions/3605861/what-is-the-graph-function-of-a-skewed-normal-distribution-curve
+    // https://cremerlab.github.io/hplc-py/methodology/fitting.html
   
 
   }
   clear_integration() {
-      this.area = 0;
-      this.start_idx = 0;
-      this.stop_idx = 0;
+    this.area = 0;
+    this.start_idx = 0;
+    this.stop_idx = 0;
   }
 };
 
 class Calibration {
   constructor() {
-      this.points = [];
-      this.type = null;
-      this.weighting = null;
-      this.coefficients = [];
-      this.units = "ppm";
+    this.points = [];
+    this.type = null;
+    this.weighting = null;
+    this.coefficients = [];
+    this.units = "ppm";
   }
 
   add_point(point) {
-      this.points.append(point);
-      this.sort_points();
+    this.points.append(point);
+    this.sort_points();
   }
 
   delete_point(level) {
-      for (let index = this.points.length - 1; index >= 0; --index) {
-          if (this.points[index].name === level) {
-              delete this.points[index];
-          }
-      }
-      this.sort_points();
+    for (let index = this.points.length - 1; index >= 0; --index) {
+        if (this.points[index].name === level) {
+            delete this.points[index];
+        }
+    }
+    this.sort_points();
   }
 
   rename_points() {
-      this.sort_points();
-      this.points = this.points.map((x, i) => x.name = i);
+    this.sort_points();
+    this.points = this.points.map((x, i) => x.name = i);
   }
 
   sort_points() {
-      this.points.sort((a, b) => a.name.localeCompare(b.name));
+    this.points.sort((a, b) => a.name.localeCompare(b.name));
   }
 
   calculate_concentration(area) {
-      if (!this.type || this.coefficients.every((i) => false)) {
-          return 0;
-      }
-      else if (this.type === "linear") {
-          return this.coefficients[0] * area + this.coefficients[1];
-      }
-      else if (this.type === "quadratic") {
-          return (this.coefficients[0] * area * area) + (this.coefficients[1] * area) + this.coefficients[2];
-      }
-      else if (this.type === "response-factor") {
-          return this.coefficients[0] * area;
-      }
+    if (!this.type || this.coefficients.every((i) => false)) {
+    return 0;
+    }
+    else if (this.type === "linear") {
+    return this.coefficients[0] * area + this.coefficients[1];
+    }
+    else if (this.type === "quadratic") {
+    return (this.coefficients[0] * area * area) + (this.coefficients[1] * area) + this.coefficients[2];
+    }
+    else if (this.type === "response-factor") {
+    return this.coefficients[0] * area;
+    }
 
-      return 0;
+    return 0;
   }
 }
 
 class calPoint {
   constructor(name, x, y) {
-      this.name = name;
-      this.x = x;
-      this.y = y;
-      this.used = True;
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.used = True;
   }
 
   set_used(use) {
-      this.used = use;
+    this.used = use;
   }
 }
 
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
-    clientside: {
+    peaks: {
         addPeak: function(numPoints, n_clicks, names, centers, heights, widths, skews, data) {
             // First initial peak added - no data yet
             if(typeof data === "undefined") {
@@ -122,20 +127,22 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
         deletePeak: function(is_deleted, vals, data) {
             var remainingIdx = [];
             var remainingPeaks = [];
-            for (ctx of dash_clientside.callback_context["states_list"]) {
+            // component gets removed in python callback function
+            // grab a list of components that still exist and rebuild peaks remaining
+            for (ctx of dash_clientside.callback_context.states_list) {
                 try {
                     for (vals of ctx) {
-                        remainingIdx.push(vals["id"]["index"])
+                        remainingIdx.push(vals.id.index)
                     }
                 } catch (error) {
                 }
             }
-            
+            // rebuild peaks using only component idx that still exist
             for (peak of data["peaks"]) {
                 if (remainingIdx.includes(peak.idx)) {
                     remainingPeaks.push(peak);
                 }
-            }
+            };
             return {"x": data["x"], "y": data["y"], "peaks": remainingPeaks};
         },
         
@@ -163,10 +170,10 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                                 break;
                         };
                     }
-                    y += peak.create_peak(data["x"]);
+                    peakY = peak.create_peak(data["x"]);
+                    y = y.map((num, idx) => {return num + peakY[idx]});
                 });
-                console.log(JSON.stringify(data["peaks"]));
-                return {"x": data["x"], "y": y, "peaks": data["peaks"]}
+                return {"x": data["x"], "y": y, "peaks": data["peaks"]};
             }
         }
     }
